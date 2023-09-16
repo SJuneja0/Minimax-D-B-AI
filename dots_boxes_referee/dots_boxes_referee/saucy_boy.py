da15d8b10a493289191b41c2c0206f7fa075f5e9
SJuneja0 <101993552+SJuneja0@users.noreply.github.com>
2023-09-16 14:56:12 -0400

    created skeleton for basic functions

--- dots_boxes_referee/dots_boxes_referee/saucy_boy.py
+++ dots_boxes_referee/dots_boxes_referee/saucy_boy.py
@@ -3,14 +3,80 @@ import referee
 
 """Functions"""
 
-#Input: The current board state
-#Output: writes a move file
-#Purpose: to perform the decided move
+
+# Input: Array of the current board-state
+# Output: Null
+# Purpose: Decides the optimal move and publishes it to the referee, this is the main function
 def decide_move():
     pass
 
-#Input: The current board state
-#Output: writes a move file
-#Purpose: to perform the decided move
-def decide_move():
+
+# Input: Two tuples (representing the points for a potential move)
+# Input: Array of the current board-state
+# Output: Boolean (true if the move suggested is valid)
+# Purpose: To check if a given move is valid
+def valid_move():
     pass
+
+
+# Input: Int of how long to run the timer
+# Output: Boolean (true when timer runs out)
+# Purpose: To tell how long a certain time has passed
+def timer():
+    pass
+
+
+"""Search Method"""
+
+
+# Input: Array of the current board-state
+# Output: Two tuples of a valid move and it's value
+# Purpose: To decide the next move
+def mini_max():
+    pass
+
+
+# Input:
+# Output:
+# Purpose:
+def alpha_beta():
+    pass
+
+
+"""Heuristics"""
+
+
+# Input:
+# Output:
+# Purpose:
+def h_control_center():
+    pass
+
+
+# Input:
+# Output:
+# Purpose:
+def h_maintain_connectivity():
+    pass
+
+
+# Input:
+# Output:
+# Purpose:
+def h_avoid_single_dots():
+    pass
+
+
+# Input:
+# Output:
+# Purpose:
+def h_counting_score():
+    pass
+
+
+# Input: Array of a suggested board state
+# Output: Int of how good that board state is for our AI
+# Purpose: To tell how good a theoretical move is
+def utility_fcn():
+    pass
+
