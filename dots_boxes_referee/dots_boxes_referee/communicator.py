a5e75ce05b8d58616aaca77dfcf4726baec84087
SJuneja0 <101993552+SJuneja0@users.noreply.github.com>
2023-09-16 18:41:25 -0400

    test commit

--- dots_boxes_referee/dots_boxes_referee/communicator.py
+++ dots_boxes_referee/dots_boxes_referee/communicator.py
@@ -1,2 +1,24 @@
 import referee
 
+"""Functions"""
+
+
+# Input: Two tuples representing the line to be played
+# Output: Null
+# Purpose: Writes a new move_file to send the move to the referee
+def write_move(point1, point2):
+    pass
+
+
+# Input: Null
+# Output: Array of current board-state
+# Purpose: Takes the newest move_file and converts it into a readable format for the AI
+def read_board():
+    pass
+
+
+# Input: Null
+# Output: Boolean (true if our turn, false if opponent's turn)
+# Purpose:to read from a file telling us if it is our turn
+def is_our_turn():
+    pass
--- dots_boxes_referee/dots_boxes_referee/saucy_boy.py
+++ dots_boxes_referee/dots_boxes_referee/saucy_boy.py
@@ -1,1 +1,16 @@
 import referee
+
+
+"""Functions"""
+
+#Input: The current board state
+#Output: writes a move file
+#Purpose: to perform the decided move
+def decide_move():
+    pass
+
+#Input: The current board state
+#Output: writes a move file
+#Purpose: to perform the decided move
+def decide_move():
+    pass
