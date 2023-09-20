141b75e165574395172f85c8ce5f785b1fa5fc4d
SJuneja0 <101993552+SJuneja0@users.noreply.github.com>
2023-09-20 18:35:19 -0400

    made skeletons for pruning functions

--- dots_boxes_referee/dots_boxes_referee/queue.py
+++ dots_boxes_referee/dots_boxes_referee/queue.py
@@ -1,5 +1,12 @@
+import tree_node
+
 class queue:
 
     def __init__(self):
         self.queue = []
 
+    def alpha_beta_pruning(self, curr_node):
+        pass
+
+    def heuristics_pruning(self):
+        pass
