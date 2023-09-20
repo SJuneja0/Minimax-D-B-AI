322865312f5c629ca9b05d809feb9086ad05f5b2
Byrnezy <97705645+Byrnezy@users.noreply.github.com>
2023-09-20 14:28:44 -0400

    I decided to look at the heuristics differently because I believe I was going down a rabbit hole. I have been making changes in both the saucy boy tester file and the board file

--- dots_boxes_referee/dots_boxes_referee/board.py
+++ dots_boxes_referee/dots_boxes_referee/board.py
@@ -1,46 +1,138 @@
+import communicator
 
+class dot:
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
+
+class Edge:
+    def __init__(self, dot1, dot2):
+        self.dot1 = dot1
+        self.dot2 = dot2
+        self.owner = None
+
+class Box:
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
+        self.owner = None
 class board:
 
     def __init__(self):
-        pass
+        self.com = communicator.communicator("GG")
+        self.board = self.com.read_board()
+        self.edges = []
+        self.dots = []
+        self.row = 9
+        self.column = 9
+        self.box = []
+        self.completed_boxes = []
 
     def getNeighbors(self, rowNum, colNum, isVertical, board):
         # neighbors is an array of 8 strings denoted as "[vertical indicator (0 or 1)]-[row number]-[col number]-[Owner]"
         # 0 is above, 1 is right, 2 is below, and 3 is left, and 4-7 are identical, except for opposing orientation
-        neighbors = [[]]
-        if(isVertical):
-            verNum = "1" #TODO changed verNum
+        neighbors = [[], [], [], [], [], [], [], []]
+        if (isVertical):
+            verNum = 1  # TODO changed verNum
             notVer = 0
         else:
             verNum = 0
             notVer = 1
-        if(rowNum > 0):
+        if (rowNum > 0):
             neighbors[0] = [verNum, (rowNum - 1), colNum, board[verNum][rowNum - 1][colNum]]
             neighbors[4] = [notVer, (rowNum - 1), colNum, board[notVer][rowNum - 1][colNum]]
-        if(rowNum < len(board[0]) - 1):
+        if (rowNum < len(board[0]) - 1):
             neighbors[2] = [verNum, (rowNum + 1), colNum, board[verNum][rowNum + 1][colNum]]
             neighbors[6] = [notVer, (rowNum + 1), colNum, board[notVer][rowNum + 1][colNum]]
-        if(colNum > 0):
+        if (colNum > 0):
             neighbors[3] = [verNum, rowNum, (colNum - 1), board[verNum][rowNum][colNum - 1]]
             neighbors[7] = [notVer, rowNum, (colNum - 1), board[notVer][rowNum][colNum - 1]]
-        if(colNum < len(board[0][0]) - 1):
+        if (colNum < len(board[0][0]) - 1):
             neighbors[1] = [verNum, rowNum, (colNum + 1), board[verNum][rowNum][colNum + 1]]
             neighbors[5] = [notVer, rowNum, (colNum + 1), board[notVer][rowNum][colNum + 1]]
         return neighbors
-        
-    
+
     def lineOwner(rowNum, colNum, isVertical, board):
-        if(isVertical):
+        if (isVertical):
             verNum = 1
         else:
             verNum = 0
         return board[verNum][rowNum][colNum]
 
-    def isTaken(rowNum, colNum, isVertical, board):
-        if(isVertical):
+    def isTaken(self, rowNum, colNum, isVertical, board):
+        if (isVertical):
             verNum = 1
         else:
             verNum = 0
-        if(board[verNum][rowNum][colNum] != ""):
+        if (board[verNum][rowNum][colNum] != ""):
             return True
         return False
+
+    def edges_controlled_by(self, player):
+        controlled_edges = []
+        for edge in self.edges:
+            if edge.owner == player:
+                controlled_edges.append(edge)
+        return controlled_edges
+
+    def get_legal_moves(self):
+        # Return a list of legal moves (unclaimed edges).
+        legal_moves = []
+        for edge in self.edges:
+            if edge.owner is None:
+                legal_moves.append(edge)
+        return legal_moves
+    # def boxes(self):
+    #     box = []
+    #     coms = communicator.communicator("GG")
+    #     board_read = coms.read_board()
+    #     count = 0
+    #
+    #     for i in range(((len(board_read[1][1]) % 2) * (len(board_read[0][1]) % 2)) - 1):
+    #         # add the lines
+    #         print(len(board_read[1][1]))
+    #         dot = self.dots() #how to
+    #         horizontals = board_read[0][count]
+    #         horizontals.append(board_read[0][count + len(board_read[0][1][1])])
+    #
+    #         verticals = board_read[1][count]
+    #         print(1)
+    #         verticals.append(board_read[1][count + len(board_read[1][1][1])])
+    #         box[count].append(horizontals, verticals)
+    #
+    #         count += 1
+    #
+    #     return box
+    def box_check(self):
+        count = 0
+        for box in self.box:
+            if (box.owner != None):
+                self.completed_boxes.append(box)
+    def generate_board(self):
+        # Create dots and edges based on the board's width and height.
+        for x in range(self.row + 1):
+            for y in range(self.column + 1):
+                self.dots.append(dot(x, y))
+
+        # Create horizontal edges.
+        for x in range(self.row):
+            for y in range(self.column + 1):
+                edge = Edge(self.dots[y + x * (self.column + 1)], self.dots[y + (x + 1) * (self.column + 1)])
+                self.edges.append(edge)
+
+        # Create vertical edges.
+        for x in range(self.row + 1):
+            for y in range(self.column):
+                edge = Edge(self.dots[y + x * (self.column + 1)], self.dots[y + (x + 1) * (self.column + 1)])
+                self.edges.append(edge)
+
+        for x in range(self.row):
+            for y in range(self.column):
+                self.box.append()                                     #need to add 4 dots
+
+    def is_game_over(self):
+        # Check if the game is over by verifying that no more legal moves are available.
+        return not any(edge.owner is None for edge in self.edges)
+
+board_ish = board()
+#print(board_ish.boxes())
--- dots_boxes_referee/dots_boxes_referee/saucy_boy_tester.py
+++ dots_boxes_referee/dots_boxes_referee/saucy_boy_tester.py
@@ -1,4 +1,3 @@
-from typing import Self
 import referee
 import communicator
 import board
@@ -51,178 +50,167 @@ def alpha_beta(time_limit):
 
 """Heuristics"""
 
-class heuristic:
+# class heuristic:
+#
+#     def __init__(self):
+#         pass
+#     # Input:
+#     # Output:
+#     # Purpose:
+#     def h_control_center(self):
+#         COM = communicator.communicator("plz")
+#         board = COM.read_board()
+#
+#         #horizontal
+#         value = 0
+#         count = 0
+#
+#         #the board is empty wtf
+#         row_num = board[1]#[-3][0]
+#         column_num = board[0][-3][1]
+#         line = [[[]]]
+#         for i in range(row_num):
+#             if (board[0][i][0] >= 1):
+#                 line[0][i].append(board[0][i][0])
+#                 #Value add 0.5
+#                 line[0][i][i] += 0.5
+#
+#         for i in range(column_num):
+#             if (board[0][i][1] >= 1):
+#                 line[0][i].append(board[0][i][1])
+#                 #Value add 0.5
+#                 line[0][i][i] += 0.5
+#
+#         #vertical
+#         value = 0
+#         row_num = board[1][-1][0]
+#         column_num = board[1][-1][1]
+#         for j in range(row_num):
+#             if (board[1][j][0] >= 1):
+#                 line[1][i+j].append(board[1][j][0])
+#                 #Value add 0.5
+#                 line[1][i+j][j] += 0.5
+#
+#         for j in range(column_num):
+#             if (board[1][j][1] >= 1):
+#                 line[1][i+j].append(board[1][j][1])
+#                 #Value add 0.5
+#                 line[1][i+j][j] += 0.5
+#
+#         pass
+#     # Input:
+#     # Output:
+#     # Purpose:
+#     def h_maintain_connectivity(self, rowNum, colNum, isVertical):
+#         coms = communicator.communicator("GG")
+#         board_read = coms.read_board()
+#         board_class = board.board()
+#         neighbors = board_class.getNeighbors(rowNum, colNum, isVertical, board_read)
+#         result = []
+#         for neighbor in neighbors:
+#             if (board_class.isTaken(neighbor[1], neighbor[2], neighbor[0], board_read) == False):
+#                 result.append(neighbor[3])
+#         return result
+#
+#
+#     # Input:
+#     # Output:
+#     # Purpose:
+#     #def h_avoid_single_dots(self):
+#         #board_read = communicator.communicator.read_board()
+#         #neighbors = board.getNeighbors()
+#         #this may not be needed to apply
+#         #pass
+#
+#
+#     # Input: null
+#     # Output: returns an array of the score
+#     # Purpose: to show what the score is
+#     def h_counting_score(self):
+#         coms = communicator.communicator("GG")
+#         board_read = coms.read_board()
+#         board_class = board.board()
+#         list_boxes, value = self.boxes()
+#         score = [0, 0]
+#         for i in range(len(list_boxes[0])):
+#             if(value[i] == 0):
+#                 score[0] += 1
+#             if (value[i] == 1):
+#                 score[1] += 1
+#
+#         return score
 
-    def __init__(self):
-        pass
-    # Input:
-    # Output:
-    # Purpose:
-    def h_control_center(self):
-        COM = communicator.communicator("plz")
-        board = COM.read_board()
 
-        #horizontal
-        value = 0
-        count = 0
+# Input: Array of a suggested board state
+# Output: Int of how good that board state is for our AI
+# Purpose: To tell how good a theoretical move is
+def utility_fcn(self):
+    ai_score = 0
+    opponent_score = 0
 
-        #the board is empty wtf
-        row_num = board[1]#[-1][0]
-        column_num = board[0][-1][1]
-        line = [[[]]]
-        for i in range(row_num):
-            if (board[0][i][0] >= 1):
-                line[0][i].append(board[0][i][0])
-                #Value add 0.5
-                line[0][i][i] += 0.5
-                
-        for i in range(column_num):
-            if (board[0][i][1] >= 1):
-                line[0][i].append(board[0][i][1])
-                #Value add 0.5
-                line[0][i][i] += 0.5  
-        
-        #vertical
-        value = 0
-        row_num = board[1][-1][0]
-        column_num = board[1][-1][1]
-        for j in range(row_num):
-            if (board[1][j][0] >= 1):
-                line[1][i+j].append(board[1][j][0])
-                #Value add 0.5
-                line[1][i+j][j] += 0.5
-                
-        for j in range(column_num):
-            if (board[1][j][1] >= 1):
-                line[1][i+j].append(board[1][j][1])
-                #Value add 0.5
-                line[1][i+j][j] += 0.5
-        
-        pass
-    # Input:
-    # Output:
-    # Purpose:
-    def h_maintain_connectivity(self, rowNum, colNum, isVertical):
-        coms = communicator.communicator("GG")
-        board_read = coms.read_board()
-        board_class = board.board()
-        print(board_read)
-        neighbors = board_class.getNeighbors(rowNum, colNum, isVertical, board_read)
-        result = []
+    # Count the number of completed boxes for each player.
+    for box in board.completed_boxes:
+        if box.owner == 'AI':
+            ai_score += 1
+        elif box.owner == 'GG':
+            opponent_score += 1
+
+    # Evaluate edge control.
+    ai_edges = len(board.edges_controlled_by('AI'))
+    opponent_edges = len(board.edges_controlled_by('Opponent'))
+
+    # Evaluate box control.
+    ai_score += ai_score - opponent_score
+
+    # Consider the number of available moves.
+    ai_moves = len(board.get_legal_moves('AI'))
+    opponent_moves = len(board.get_legal_moves('Opponent'))
+
+    # Combine and balance the factors in your heuristic.
+    heuristic_score = (ai_score - opponent_score) + (ai_edges - opponent_edges) + (ai_moves - opponent_moves)
+
+    return heuristic_score
+
+#*if __name__ == "__main__":
+    #try:
+    #    pass
+    #except:
+     #   pass
 
 
-        pass
-        
-        pass
-    
 
-    # Input:
-    # Output:
-    # Purpose:
-    def h_avoid_single_dots():
-        board_read = communicator.communicator.read_board()
-        neighbors = board.getNeighbors()
-        #this may not be needed to apply
-        pass
+def boxes(self):
+    value = []
+    count = 0
 
+    coms = communicator.communicator("GG")
+    board_read = coms.read_board()
+    board_class = board.board()
+    box = [[]]
 
-    # Input: null
-    # Output: returns an array of the score
-    # Purpose: to show what the score is
-    def h_counting_score(self):
-        list_boxes, value = self.boxes()
-        score = [0,0]
-        for i in len(list_boxes):
-            if(value[i] == 0):
-                score[0] += 1
-            if (value[i] == 1):
-                score[1] += 1
-        
-        return score
+    for i in range(((len(board_read[1][1])%2) * (len(board_read[0][1])%2)) - 1):
+        #add the lines
+        print(len(board_read[1][1]))
+        horizontals = [board_read[0][count]]
+        horizontals.append([board_read[0][count+len(board_read[0][1][1])]])
 
+        verticals = [board_read[1][count]]
+        print(count)
+        print(((len(board_read[1][1])%2) * (len(board_read[0][1])%2)) - 1)
+        verticals.append([board_read[1][count+len(board_read[1][1][1])]])
+        box[count].append([horizontals, verticals])
 
-    # Input: Array of a suggested board state
-    # Output: Int of how good that board state is for our AI
-    # Purpose: To tell how good a theoretical move is
-    def utility_fcn():
-        pass
+        #find out how to count a score
+        #value[i] = 1  # 0 and 1 to distingish between self or opponent. 2 as the preinput
 
-    #*if __name__ == "__main__":
-        #try:
-        #    pass
-        #except:
-         #   pass
+        count += 1
+        value[i] = 1
 
-    
+    return box, value
 
-    def boxes(self):
-        value = []
-        count = 0
-        box = [[]]
-        for i in range(64):
-            #add the lines
-            horizontals = board[0][count]
-            horizontals.append(board[0][count+9])
-
-            verticals = board[1][count]
-            verticals.append(board[1][count+9])
-            box[count].append([horizontals, verticals])
-            for i in range(2):
-                #find who owns all the lines and adjust the value
-                pass
-            #find out how to count a score
-            value[i] = 1 # 0 and 1 to distingish between self or opponent. 2 as the preinput
-
-            count += 1
-        
-        return box & value
-
-test = heuristic()
+#test = heuristic()
 board_test = board.board
 #test.boxes()
 #test.h_counting_score()
-test.h_maintain_connectivity(1, 1, 1)
+#test.h_maintain_connectivity(1, 1, 1)
 #test.utility_fcn()
-
-# for lines in line:
-#                 try:
-#                     if(isVertical):
-#                         if (lines == board[1][rowNum][colNum - 1]): #top 
-#                             if (board_class.isTaken(rowNum, colNum - 1, isVertical, board) == False):
-#                                 result.append(lines)
-#                         if (lines == board[1][rowNum][colNum + 1]):
-#                             if (board_class.isTaken(rowNum, colNum + 1, isVertical, board) == False):
-#                                 result.append(lines)
-#                         if (lines == board[0][rowNum][colNum - 1]):
-#                             if (board_class.isTaken(rowNum, colNum - 1, 0, board) == False):
-#                                 result.append(lines)
-#                         if (lines == board[0][rowNum][colNum]):
-#                             if (board_class.isTaken(rowNum, colNum, 0, board) == False):
-#                                 result.append(lines)
-#                         if (lines == board[0][rowNum + 1][colNum - 1]):
-#                             if (board_class.isTaken(rowNum + 1, colNum - 1, 0, board) == False):
-#                                 result.append(lines)
-#                         if (lines == board[0][rowNum + 1][colNum]):
-#                             if (board_class.isTaken(rowNum + 1, colNum, 0, board) == False):
-#                                 result.append(lines)
-#                     else:
-#                         if (lines == board[0][rowNum][colNum - 1]):
-#                             if (board_class.isTaken(rowNum, colNum - 1, 0, board) == False):
-#                                 result.append(lines)
-#                         if (lines == board[0][rowNum -1 ][colNum + 1]):
-#                             if (board_class.isTaken(rowNum - 1, colNum + 1, 0, board) == False):
-#                                 result.append(lines)
-#                         if (lines == board[1][rowNum - 1][colNum]):
-#                             if (board_class.isTaken(rowNum - 1, colNum, 1, board) == False):
-#                                 result.append(lines)
-#                         if (lines == board[1][rowNum - 1][colNum + 1]):
-#                             if (board_class.isTaken(rowNum - 1, colNum + 1, 1, board) == False):
-#                                 result.append(lines)
-#                         if (lines == board[1][rowNum][colNum + 1]):
-#                             if (board_class.isTaken(rowNum, colNum + 1, 1, board) == False):
-#                                 result.append(lines)
-#                         if (lines == board[1][rowNum][colNum]):
-#                             if (board_class.isTaken(rowNum, colNum, 1, board) == False):
-#                                 result.append(lines)
-#                 except:
-#                     pass
