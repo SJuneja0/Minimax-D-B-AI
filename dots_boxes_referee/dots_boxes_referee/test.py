a09d8b14fbcc74f99711334863f26a563305930c
jp3wi <jpwilusz@wpi.edu>
2023-09-21 18:25:11 -0400

    Created unit test file and deleted temporary test file

---
+++ dots_boxes_referee/dots_boxes_referee/test.py
@@ -0,0 +1,13 @@
+import saucy_boy
+import board
+import queue
+import tree_node
+import communicator
+
+import unittest
+
+class testFileMethods(unittest.TestCase):
+    # For tests, use functions that use AssertEquals, etc.
+    def defaultTest(self):
+        self.assertTrue(True)
+    
