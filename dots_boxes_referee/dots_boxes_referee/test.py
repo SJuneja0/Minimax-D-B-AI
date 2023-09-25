0ae41ccab762e6791d1d59e9673e6128bbb13758
jp3wi <jpwilusz@wpi.edu>
2023-09-25 12:59:56 -0400

    Started developing tests

--- dots_boxes_referee/dots_boxes_referee/test.py
+++ dots_boxes_referee/dots_boxes_referee/test.py
@@ -1,6 +1,5 @@
 import saucy_boy
 import board
-import queue
 import tree_node
 import communicator
 
@@ -11,3 +10,24 @@ class testFileMethods(unittest.TestCase):
     def defaultTest(self):
         self.assertTrue(True)
     
+    def test_board_functions(self, bd):
+        test_board = board(9, 9, "Test", "Test Opponent")
+        bd.create_board()
+        self.assertEqual(test_board.dots, bd.dots)
+        self.assertEqual(test_board.edges, bd.edges)
+        self.assertEqual(test_board.box, bd.box)
+        self.assertFalse(test_board.is_game_over())
+        test_legal_moves = test_board.get_legal_moves()
+        # Unsure how to test update_board
+    
+    def test_saucy_boy_functions(self, sb):
+        test_sb = saucy_boy("Test Opponent")
+
+        self.assertTrue(True)
+
+    def test_tree_node_construct_path(self, tn_root, tn_child):
+
+        self.assertTrue(True)
+
+    def test_communicator_functions(self, comm):
+        self.assertTrue(True)
